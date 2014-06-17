2d3544396fa795273af7882d80e15cd9f5fd793e
nick.robertson <nick.robertson@octanner.com>
2014-06-17 14:08:23 -0600

    Added Ajax on vote count

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,16 +14,19 @@ class CommentsController < ApplicationController
     end
   end
   def vote
-    comment = Comment.find(params[:id])
-    vote = Vote.create(voteable: comment, creator: current_user, vote: params[:vote])
+    @comment = Comment.find(params[:id])
+    @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
 
-
-    if vote.valid?
-      flash[:notice] = "Your vote was counted."
-    else
-      flash[:error] = "Your vote was not counted, you can only vote once."
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = "Your vote was counted."
+        else
+          flash[:error] = "Your vote was not counted, you can only vote once."
+        end
+        redirect_to :back
+      end
+      format.js
     end
-
-    redirect_to :back
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,13 +41,17 @@ class PostsController < ApplicationController
   def vote
     @vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
 
-    if @vote.valid?
-      flash[:notice] = "Your vote was counted."
-    else
-      flash[:error] = "Your vote was not counted, you can only vote once."
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = "Your vote was counted."
+        else
+          flash[:error] = "Your vote was not counted, you can only vote once."
+        end
+        redirect_to :back
+      end
+      format.js
     end
-
-    redirect_to :back
   end
 
   private
